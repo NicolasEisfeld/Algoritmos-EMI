95810fb61f598ed7c88f2252d346205b5a7b0bfc
Nicolas1818 <96309633+NicolasEisfeld@users.noreply.github.com>
2024-04-03 09:44:21 -0300

    Create Velocidade.java

---
+++ Leitura/Velocidade.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Velocidade {
+  public static void main(String [] args);
+  Scanner leitura1 = new Scanner(System.in);
+  System.out.print("Tempo em horas: ")
+  double duracao_horas = leitura1.nextDouble():
+
+  Scanner leitura2 = new Scanner(System.in);
+  double abastecer1 = leitura2.nextDouble();
+  double abastecer2 = leitura2.nextDouble();
+  
