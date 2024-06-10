1d01d6a494b2ce11eb95b446c04efb8f0711d224
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-06-10 17:01:41 -0300

    Update Sequencia.java

--- Repetições/Lista2/Sequencia.java
+++ Repetições/Lista2/Sequencia.java
@@ -1,4 +1,13 @@
 import java.util.Scanner;
+
 public class Sequencia {
-    
+   public static void main(String [] args) {
+   Scanner dado = new Scanner(System.in);
+   int valor = dado.nextInt();
+   int max = valor + 10;
+   while (valor < max) {
+           valor++;
+           System.out.println(valor);
+        }       
+   }
 }
