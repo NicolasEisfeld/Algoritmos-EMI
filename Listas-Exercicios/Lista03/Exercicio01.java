24e514ca6cf24045fa9a2bdfe38ae48aca6d088c
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-06-26 14:15:36 -0300

    Update Exercicio01.java

--- Listas-Exercicios/Lista03/Exercicio01.java
+++ Listas-Exercicios/Lista03/Exercicio01.java
@@ -13,7 +13,7 @@ public class Exercicio01 {
     public static void main(String[] args){
         
         for(int i=1, j=60; j>=0; i+=3, j-=5) {
-            System.out.println("I="+ i + "J="+ j);
+            System.out.println("I="+ i + " J="+ j);
         }
     }
 }
