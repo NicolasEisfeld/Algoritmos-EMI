a556665fb861e179fe615fb99935307a4e1e55b2
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-06-26 14:13:23 -0300

    Update Exercicio03.java

--- Listas-Exercicios/Lista03/Exercicio03.java
+++ Listas-Exercicios/Lista03/Exercicio03.java
@@ -19,7 +19,7 @@ public class Exercicio03 {
     public static void main(String [] args) {
         for(double i=0; i<=2; i+=0.2) {
             for(double j=1+i; j<=3+i; j+=1.0) {
-                System.out.println("I="+ i + "J=" + j)
+                System.out.println("I="+ i + " J=" + j);
             }
         }
     }
