296cfecde044c5d6d000b196e9fe686631bc4454
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-06-27 07:50:58 -0300

    Update and rename Vetores.java to Vetores.java

---
+++ Estrutura-de-Dados/Vetores/Vetores.java
@@ -0,0 +1,9 @@
+
+public class Vetores {
+    public static void main(String [] args) {
+        int n[] = {1, 2, 3, 4, 5};
+        for(int valor: n) {
+            System.out.println(valor);
+        }
+    }
+}
