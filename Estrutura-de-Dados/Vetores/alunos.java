f51a1e685a305c17aa203ff814d59beaf2da4a3e
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-07-03 22:46:37 -0300

    Update alunos.java

--- Estrutura-de-Dados/Vetores/alunos.java
+++ Estrutura-de-Dados/Vetores/alunos.java
@@ -9,17 +9,27 @@ public class Alunos {
         double notas[][] = new double[10][2];
         double media_aluno[] = new double[10];
         double media_prova[] = new double[2];
+        double min_prova = 0;
+        double max_prova = 0;
+        double min_media = Integer.MAX_VALUE;
+        double max_media = Integer.MIN_VALUE;
         
         for(int i=0; i<10; i++) {
             nomes[i] = dado.nextLine();
             System.out.println("Notas de " + nomes[i]);
             for(int j=0; j < 2; j++) {
                 notas[i][j] = dado.nextDouble();
-                media_aluno[i] += notas[i][j];
+                media_aluno[i] += notas[i][j];                
                 media_prova[j] += notas[i][j];
                 
             }
             media_aluno[i] /= 2.0;
+            if(media_aluno[i] < min_media) {
+                min_media = media_aluno[i];
+            }
+            if(notas[i][j] > max_media) {
+                max_media = media_aluno[i];
+            }            
             
             System.out.println(media_aluno[i]);
             
@@ -33,11 +43,23 @@ public class Alunos {
         System.out.println(media_prova[0]);
         media_prova[1] /= 10.0;
         System.out.println(media_prova[1]);
+        if(media_prova[0] == media_prova[1]) {}
+        else {
+            if(media_prova[0] > media_prova[1]) {
+                max_prova = media_prova[0];
+                min_prova = media_prova[1];
+            } else {
+                max_prova = media_prova[1];
+                min_prova = media_prova[0];
+            }
+        }
         
         double media_geral = (media_prova[0] + media_prova[1]) / 2.0;
         System.out.println(media_geral);
-        // fazer minima, maxima das medias
-        // fazer '' por prova
+        System.out.println(min_media);
+        System.out.println(max_media);
+        System.out.println(min_prova);
+        System.out.println(max_prova);
         
     }
 }
