13266bc7120954bc9f6b7a1b4f776770b52d0325
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-07-31 15:07:17 -0300

    Add files via upload

---
+++ Metodos/Calculo_Raizes.java
@@ -0,0 +1,20 @@
+
+package Metodos;
+import java.util.Scanner;
+
+public class Calculo_Raizes {
+    private static Scanner dado = new Scanner(System.in);    
+    private static double a, b, c;
+    public static double[] raizes (double a, double b, double c) { 
+        a = dado.nextDouble();
+        b = dado.nextDouble();
+        c = dado.nextDouble();
+        if (a == 0) {
+            System.out.println("Resposta Invalida");
+            return null;
+        } else {}
+
+        
+        return raizes[1];
+    }
+}
