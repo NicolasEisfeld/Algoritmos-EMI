093982e16a6c493ec75e212092698e0f7d5231b9
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-08-05 22:24:00 -0300

    Update Bhaskara.java

--- Metodos/Bhaskara.java
+++ Metodos/Bhaskara.java
@@ -7,30 +7,26 @@ public class Bhaskara {
     public static double raizes[];
     
     public static void Entrada() {
-        Scanner dado = new Scanner(System.in);
-        System.out.print("Valor de A: ");
-        a = dado.nextDouble();
-        System.out.print("Valor de B: ");
-        b = dado.nextDouble();
-        System.out.print("Valor de C: ");
-        c = dado.nextDouble();
-        delta = (Math.pow(b, 2) - (4 * a * c));
+
         
         
     }
-    public static void Calculo() {
-        if(delta < 0 || a == 0) {
+
+    public static double[] Calculo(double a, double b, double c) {
+        double delta = b * b - 4 * a * c;
+        
+        if (delta < 0 || a == 0) {
             System.out.println("Não existem resultados reais");
-            
-            double raizes[] = null;
-            
+            return null;
         }
+        
         else if( a != 0) {
             
         
             if(delta == 0 ) {
                 double raizes[] = new double[1];
                 raizes[0] = - b / (2 * a);
+                
                 
 
             }
@@ -39,16 +35,30 @@ public class Bhaskara {
                 raizes[0] = (- b + Math.sqrt(delta))/ (2 * a);
                 raizes[1] = (- b - Math.sqrt(delta))/ (2 * a);
                 
+                
             }
         }
+        return raizes; // Esse return tá ruim
         
     }
     
     public static void main(String [] args) {
-        Entrada();
-        Calculo();
-        for(int i = 0; i < raizes.length; i++) {
-            System.out.println("Raiz "+ i +": " + raizes[i]);
+        Scanner dado = new Scanner(System.in);
+        System.out.print("Valor de A: ");
+        a = dado.nextDouble();
+        System.out.print("Valor de B: ");
+        b = dado.nextDouble();
+        System.out.print("Valor de C: ");
+        c = dado.nextDouble();
+        Calculo(a, b, c); // Até aqui não deu pau o código
+        if(raizes.length == (2)) {
+            for(int i = 0; i < 2; i++) {
+                System.out.println("Raiz "+ i +": " + raizes[i]);
+            }
+        } else{
+            for(int i = 0; i < 1; i++) {
+                System.out.println("Raiz "+ i +": " + raizes[i]);
+            }
         }
        
     }
