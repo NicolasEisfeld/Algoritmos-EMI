1a4eba5ef8f4f9c631228cf39030420ef7624128
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-08-19 22:39:29 -0300

    Update and rename Exercicio9.java to Exercicio09.java

--- Listas-Exercicios/Lista06/Exercicio09.java
+++ Listas-Exercicios/Lista06/Exercicio09.java
@@ -8,7 +8,7 @@ package Lista_Subrotinas;
 
 import java.util.Scanner;
 
-public class Exercicio9 {
+public class Exercicio09 {
 
     public static void main(String[] args) {
         Scanner dado = new Scanner(System.in);
