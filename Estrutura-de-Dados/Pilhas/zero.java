5057729360671e5b612f9bd840802c99b6d89547
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-08-28 15:05:06 -0300

    Add files via upload

--- Estrutura-de-Dados/Pilhas/zero.java
+++ Estrutura-de-Dados/Pilhas/zero.java
@@ -8,12 +8,19 @@ public class zero {
 
     public static void main(String[] args) {
         Scanner dado = new Scanner(System.in);
-        int N = dado.nextInt();
-        
+        int N = dado.nextInt(), soma = 0;
+        Stack< Integer > Valores = new Stack();
         for(int i = 0; i < N; i++) {
-           
+           Valores.push(dado.nextInt());
         }
-        
+        for(int i = 0; i < N; i++) {
+            if(Valores.push(i) == 0) {
+                Valores.pop();
+            } 
+            soma += Valores.push(i);
+            
+        }
+        System.out.println(soma);
     }
     
 }
