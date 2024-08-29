ddea968cfc2b946822fd2e2af57c9ea178fc40ad
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-08-29 11:35:13 -0300

    Add files via upload

---
+++ Torre_Hanoi/Torre_Hanoi.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/* Autor: Nicolas Eisfeld Ferreira
+https://github.com/NicolasEisfeld/ */
+
+/*
+    Deve ser programado um algoritmo em Java da Torre de Hanói, com um número de discos informado pelo usuário. 
+    O programa deve informar qual o número mínimo de movimentos necessários e listar os movimentos necessários para resolver a Torre com o número informado.
+
+*/
+
+public class Torre_Hanoi {
+
+    public static void main(String[] args) {
+        Scanner dado = new Scanner(System.in);
+        int N_Discos = dado.nextInt(); // Quantidade de Discos
+        double N_Minimo = Math.pow(2, N_Discos) - 1;
+        System.out.println(N_Minimo); // Exibir número mínimo de jogadas
+        
+    }
+    
+}
