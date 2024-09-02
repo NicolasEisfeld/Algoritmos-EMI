88f82e770909e7caca92952b9e9af7a41b174c15
NicolasEisfeld <96309633+NicolasEisfeld@users.noreply.github.com>
2024-09-02 19:55:36 -0300

    Update Torre_Hanoi.java

--- Torre_Hanoi/Torre_Hanoi.java
+++ Torre_Hanoi/Torre_Hanoi.java
@@ -14,19 +14,19 @@ public class Torre_Hanoi {
     
     public static void main(String[] args) {
         Scanner dado = new Scanner(System.in);
-        int N_Discos = dado.nextInt(); // Quantidade de Discos
-        double N_Minimo = Math.pow(2, N_Discos) - 1;
-        System.out.println(N_Minimo); // Exibir número mínimo de jogadas
-        Hanoi(N_Discos, "T1", "T2", "T3"); // Chama o método recursivo
-        System.out.println(movimento); // Exibe o return do método
+        int Discos = dado.nextInt(); // Quantidade de Discos
+        double N_Minimo = Math.pow(2, Discos) - 1;
+        System.out.println("O numero minimo de jogadas possíveis eh: " + N_Minimo); // Exibir número mínimo de jogadas
+        Hanoi(Discos, "T1", "T2", "T3"); // Chama o método recursivo
         
     }
-    public static Hanoi (int Discos, String Origem, String Auxiliar, String Destino) {
-        if (Discos > 0) { // Solução através da recursividade 
-            Hanoi(Discos-1, Origem, Destino, Auxiliar); // Os movimentos da origem para auxiliar (n-1)
-            System.out.println("Mover Disco "+ Discos + " de " + Origem + "para " + Destino);
-            Hanoi(Discos-1, Auxiliar, Origem, Destino); // Os movimentos do auxiliar para o destino (n-1)
-        }
+    public static void Hanoi (int Discos, String Origem, String Auxiliar, String Destino) {
+        if (Discos == 1) {System.out.println("Mover Disco "+ Discos + " de " + Origem + " para " + Destino) ; return;}
+        // Solução através da recursividade 
+        Hanoi(Discos-1, Origem, Destino, Auxiliar); // Os movimentos da origem para auxiliar (n-1)
+        System.out.println("Mover Disco "+ Discos + " de " + Origem + " para " + Destino);
+        Hanoi(Discos-1, Auxiliar, Origem, Destino); // Os movimentos do auxiliar para o destino (n-1)
+        
     }
     
 }
